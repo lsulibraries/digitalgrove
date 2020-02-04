5b4faafae856bdccc6d9ac7dfab445513b3979a8
ktanglao <ktanglao@lsu.edu>
2020-02-04 13:20:03 -0600

    Adds document language to html pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,7 +147,7 @@
 				'<div id="navPanel">' +
 					'<nav>' +
 					'</nav>' +
-					'<a href="#navPanel" class="close"></a>' +
+					'<a href="#navPanel" class="close"> </a>' +
 				'</div>'
 			)
 				.appendTo($body)
