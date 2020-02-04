910ef47d03c1534c9b97bca5b2f5593f35e3c575
Ubuntu <ubuntu@ip-172-31-31-50.us-west-2.compute.internal>
2020-02-04 18:42:16 +0000

    init

---
+++ assets/js/consent_overlay.js
@@ -0,0 +1,23 @@
+<!-- Begin Consent Overlay -->
+<link href="https://www.lsu.edu/_resources/css/consent.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="https://www.lsu.edu/_resources/js/consent.js"></script>
+<script type="text/javascript">// <![CDATA[
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+ "palette": {
+ "popup": {
+ "background": "#fdd023",
+ "text": "#333333"
+ },
+ "button": {
+ "background": "#efefef",
+ "text": "#461d7c"
+ }
+ },
+ "content": {
+ "link": "LSU Privacy Statement.",
+ "href": "/privacy/"
+ }
+})});
+// ]]></script>
+<!-- End Consent Overlay -->
